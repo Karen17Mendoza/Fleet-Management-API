b480967506ec7efdaf121976b79ce241236c5c7d
Karen17Mendoza <melanie171095@gmail.com>
2024-09-13 10:25:24 -0500

    first commit

---
+++ src/test/java/com/example/Fleet/Management/API/FleetManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fleet.Management.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
